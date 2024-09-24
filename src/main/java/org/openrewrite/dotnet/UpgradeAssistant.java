99f3b9d10e8c0cfbc090a83e01adbd63fac964de
Bryce Tompkins <bryce@moderne.io>
2024-09-24 16:44:20 -0400

    Add UpgradeApplicationAnalysis recipe

--- src/main/java/org/openrewrite/dotnet/UpgradeAssistant.java
+++ src/main/java/org/openrewrite/dotnet/UpgradeAssistant.java
@@ -15,40 +15,25 @@
  */
 package org.openrewrite.dotnet;
 
-import lombok.*;
-import org.jspecify.annotations.Nullable;
-import org.openrewrite.*;
-import org.openrewrite.quark.Quark;
-import org.openrewrite.scheduling.WorkingDirectoryExecutionContextView;
-import org.openrewrite.text.PlainText;
-import org.openrewrite.tree.ParseError;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
 
 import java.io.BufferedReader;
-import java.io.File;
 import java.io.IOException;
-import java.io.UncheckedIOException;
-import java.nio.charset.Charset;
-import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.*;
-import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Value
 @EqualsAndHashCode(callSuper = false)
-public class UpgradeAssistant extends ScanningRecipe<UpgradeAssistant.Accumulator> {
-    private static final String FIRST_RECIPE = UpgradeAssistant.class.getName() + ".FIRST_RECIPE";
-    private static final String PREVIOUS_RECIPE = UpgradeAssistant.class.getName() + ".PREVIOUS_RECIPE";
-    private static final String INIT_REPO_DIR = UpgradeAssistant.class.getName() + ".INIT_REPO_DIR";
-    private static final String UPGRADE_ASSISTANT = "upgrade-assistant";
-    private static final String DOTNET_HOME = System.getProperty("user.home") + File.separator + ".dotnet";
+public class UpgradeAssistant extends UpgradeAssistantRecipe {
 
     @Option(
             displayName = "Target framework version",
             description = "Target framework to which source project should be upgraded.",
             example = "net9.0")
-    @Nullable
     String targetFramework;
 
     @Override
@@ -63,124 +48,7 @@ public class UpgradeAssistant extends ScanningRecipe<UpgradeAssistant.Accumulato
     }
 
     @Override
-    public Accumulator getInitialValue(ExecutionContext ctx) {
-        Path directory = createDirectory(ctx);
-        if (ctx.getMessage(INIT_REPO_DIR) == null) {
-            ctx.putMessage(INIT_REPO_DIR, directory);
-            ctx.putMessage(FIRST_RECIPE, ctx.getCycleDetails().getRecipePosition());
-        }
-        return new Accumulator(directory);
-    }
-
-    @Override
-    public TreeVisitor<?, ExecutionContext> getScanner(Accumulator acc) {
-        return new TreeVisitor<Tree, ExecutionContext>() {
-            @Override
-            public @Nullable Tree visit(@Nullable Tree tree, ExecutionContext ctx) {
-                if (tree instanceof SourceFile && !(tree instanceof Quark) && !(tree instanceof ParseError)) {
-                    SourceFile sourceFile = (SourceFile) tree;
-
-                    // Only extract initial source files for first upgrade-assistant recipe
-                    if (Objects.equals(ctx.getMessage(FIRST_RECIPE), ctx.getCycleDetails().getRecipePosition())) {
-                        acc.writeSource(sourceFile);
-                    }
-                }
-                return tree;
-            }
-        };
-    }
-
-    @Override
-    public Collection<? extends SourceFile> generate(Accumulator acc, ExecutionContext ctx) {
-        Path previous = ctx.getMessage(PREVIOUS_RECIPE);
-        if (previous != null &&
-                !Objects.equals(ctx.getMessage(FIRST_RECIPE), ctx.getCycleDetails().getRecipePosition())) {
-            acc.copyFromPrevious(previous);
-        }
-        if (ctx.getCycle() == 1) {
-            // upgrade-assistant run more than once on a project will log an "Unknown target framework" message
-            runUpgradeAssistant(acc, ctx);
-        }
-        ctx.putMessage(PREVIOUS_RECIPE, acc.getDirectory());
-
-        // FIXME check for generated files
-        return Collections.emptyList();
-    }
-
-    private void runUpgradeAssistant(Accumulator acc, ExecutionContext ctx) {
-        Map<String, String> env = buildUpgradeAssistantEnv();
-
-        for (Path projectFile : acc.getProjectFiles()) {
-
-            List<String> command = buildUpgradeAssistantCommand(projectFile);
-            Path out = null;
-            Path err = null;
-
-            try {
-                ProcessBuilder builder = new ProcessBuilder();
-                builder.command(command);
-                builder.directory(acc.getDirectory().toFile());
-                env.forEach(builder.environment()::put);
-
-                out = Files.createTempFile(
-                        WorkingDirectoryExecutionContextView.view(ctx).getWorkingDirectory(),
-                        UPGRADE_ASSISTANT,
-                        null);
-                err = Files.createTempFile(
-                        WorkingDirectoryExecutionContextView.view(ctx).getWorkingDirectory(),
-                        UPGRADE_ASSISTANT,
-                        null);
-                builder.redirectOutput(ProcessBuilder.Redirect.to(out.toFile()));
-                builder.redirectError(ProcessBuilder.Redirect.to(err.toFile()));
-
-                Process process = builder.start();
-                process.waitFor(20, TimeUnit.MINUTES);
-                if (process.exitValue() != 0) {
-                    String error = "Command failed: " + String.join(" ", command);
-                    if (Files.exists(err)) {
-                        error += "\n" + new String(Files.readAllBytes(err));
-                    }
-                    throw new RuntimeException(error);
-                } else {
-                    for (Map.Entry<Path, Long> entry : acc.beforeModificationTimestamps.entrySet()) {
-                        Path path = entry.getKey();
-                        if (!Files.exists(path) || Files.getLastModifiedTime(path).toMillis() > entry.getValue()) {
-                            acc.modified(path);
-                        }
-                    }
-                    processOutput(projectFile, out, acc);
-                }
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            } finally {
-                deleteFile(out);
-                deleteFile(err);
-            }
-        }
-    }
-
-    private Map<String, String> buildUpgradeAssistantEnv() {
-        Map<String, String> env = new HashMap<>();
-        env.put("TERM", "dumb");
-        String path = System.getenv("PATH");
-        // This is required to find .NET SDKs
-        env.put("PATH", path + File.pathSeparator + DOTNET_HOME);
-        return env;
-    }
-
-    private void deleteFile(@Nullable Path path) {
-        if (path != null) {
-            try {
-                Files.deleteIfExists(path);
-            } catch (IOException e) {
-                // FIXME recipe logger?
-            }
-        }
-    }
-
-    private List<String> buildUpgradeAssistantCommand(Path projectFile) {
+    public List<String> buildUpgradeAssistantCommand(Accumulator acc, ExecutionContext ctx, Path projectFile) {
         List<String> command = new ArrayList<>();
         command.add(getUpgradeAssistantPath().toString());
         command.add("upgrade");
@@ -188,36 +56,21 @@ public class UpgradeAssistant extends ScanningRecipe<UpgradeAssistant.Accumulato
         command.add("--non-interactive");
         command.add("--operation");
         command.add("Inplace");
-        if (targetFramework != null) {
-            command.add("--targetFramework");
-            command.add(targetFramework);
-        }
+        command.add("--targetFramework");
+        command.add(targetFramework);
         return command;
     }
 
-    private Path getUpgradeAssistantPath() {
-        String cmdName = UPGRADE_ASSISTANT;
-        if (System.getProperty("os.name").contains("Windows")) {
-            cmdName += ".exe";
-        }
+    @Override
+    public void runUpgradeAssistant(Accumulator acc, ExecutionContext ctx) {
+        for (Path projectFile : acc.getProjectFiles()) {
+            execUpgradeAssistant(projectFile, acc, ctx);
 
-        // Look for upgrade-assistant in conventional installation locations
-        Path cmdPath = Paths.get(DOTNET_HOME).resolve("tools").resolve(cmdName);
-        if (Files.exists(cmdPath)) {
-            return cmdPath;
         }
-
-        for (String path : System.getenv("PATH").split(File.pathSeparator)) {
-            cmdPath = Paths.get(path).resolve(cmdName);
-            if (Files.exists(cmdPath)) {
-                return cmdPath;
-            }
-        }
-
-        throw new IllegalStateException("Unable to find " + cmdName + " on PATH");
     }
 
-    private void processOutput(Path projectFile, Path output, Accumulator acc) {
+    @Override
+    protected void processOutput(Path projectFile, Path output, Accumulator acc) {
         Path projectDir = projectFile.getParent();
 
         try (BufferedReader reader = Files.newBufferedReader(output)) {
@@ -251,151 +104,6 @@ public class UpgradeAssistant extends ScanningRecipe<UpgradeAssistant.Accumulato
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
-        }
-    }
-
-    @Override
-    public TreeVisitor<?, ExecutionContext> getVisitor(Accumulator acc) {
-        return new TreeVisitor<Tree, ExecutionContext>() {
-            @Override
-            public @Nullable Tree visit(@Nullable Tree tree, ExecutionContext ctx) {
-                if (tree instanceof SourceFile) {
-                    SourceFile sourceFile = (SourceFile) tree;
-                    return createAfter(sourceFile, acc);
-                }
-                return tree;
-            }
-        };
-    }
-
-    private SourceFile createAfter(SourceFile before, Accumulator acc) {
-        String error = acc.getFileError(acc.resolvedPath(before));
-        if (error != null) {
-            throw new RecipeException(error);
-        }
-
-        if (!acc.wasModified(before)) {
-            return before;
-        }
-
-        return new PlainText(
-                before.getId(),
-                before.getSourcePath(),
-                before.getMarkers(),
-                Optional.ofNullable(before.getCharset()).map(Charset::name).orElse(null),
-                before.isCharsetBomMarked(),
-                before.getFileAttributes(),
-                null,
-                acc.content(before),
-                Collections.emptyList());
-    }
-
-    private static Path createDirectory(ExecutionContext ctx) {
-        WorkingDirectoryExecutionContextView view = WorkingDirectoryExecutionContextView.view(ctx);
-        return Optional.of(view.getWorkingDirectory()).map(d -> d.resolve("repo")).map(d -> {
-            try {
-                return Files.createDirectory(d).toRealPath();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }).orElseThrow(() -> new IllegalStateException("Failed to create working directory for repo"));
-    }
-
-    @ToString
-    @EqualsAndHashCode
-    @RequiredArgsConstructor
-    public static class Accumulator {
-        @Getter
-        private final Path directory;
-
-        private final Map<Path, String> fileErrors = new HashMap<>();
-        final Map<Path, Long> beforeModificationTimestamps = new HashMap<>();
-        final Set<Path> modified = new LinkedHashSet<>();
-
-        @Getter
-        private final List<Path> projectFiles = new ArrayList<>();
-
-        private void copyFromPrevious(Path previous) {
-            try {
-                Files.walkFileTree(previous, new SimpleFileVisitor<Path>() {
-                    @Override
-                    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
-                        Path target = directory.resolve(previous.relativize(dir));
-                        if (!target.equals(directory)) {
-                            Files.createDirectory(target);
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-
-                    @Override
-                    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
-                        try {
-                            Path target = directory.resolve(previous.relativize(file));
-                            Files.copy(file, target);
-                            beforeModificationTimestamps.put(target, Files.getLastModifiedTime(target).toMillis());
-                        } catch (NoSuchFileException ignore) {
-                        }
-                        return FileVisitResult.CONTINUE;
-                    }
-                });
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-
-        private void writeSource(SourceFile sourceFile) {
-            try {
-                Path path = resolvedPath(sourceFile);
-                Files.createDirectories(path.getParent());
-                PrintOutputCapture.MarkerPrinter markerPrinter = new PrintOutputCapture.MarkerPrinter() {
-                };
-                Path written = Files.write(
-                        path,
-                        sourceFile.printAll(new PrintOutputCapture<>(0, markerPrinter))
-                                .getBytes(Optional.ofNullable(sourceFile.getCharset()).orElse(StandardCharsets.UTF_8)));
-                beforeModificationTimestamps.put(written, Files.getLastModifiedTime(written).toMillis());
-                if (isProjectFile(written)) {
-                    projectFiles.add(written);
-                }
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-
-        private boolean isProjectFile(Path path) {
-            String pathString = path.toString();
-            return pathString.endsWith(".csproj") || pathString.endsWith(".vbproj");
-        }
-
-        private void modified(Path path) {
-            modified.add(path);
-        }
-
-        private boolean wasModified(SourceFile tree) {
-            return modified.contains(resolvedPath(tree));
-        }
-
-        private String content(SourceFile tree) {
-            try {
-                Path path = resolvedPath(tree);
-                return tree.getCharset() != null ?
-                        new String(Files.readAllBytes(path), tree.getCharset()) :
-                        new String(Files.readAllBytes(path));
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-        }
-
-        private Path resolvedPath(SourceFile tree) {
-            return directory.resolve(tree.getSourcePath());
-        }
-
-        private void addFileError(Path path, String error) {
-            fileErrors.put(path, error);
-        }
-
-        private @Nullable String getFileError(Path path) {
-            return fileErrors.get(path);
         }
     }
 }
---
+++ src/main/java/org/openrewrite/dotnet/UpgradeAssistantAnalyze.java
@@ -0,0 +1,184 @@
+/*
+ * Copyright 2024 the original author or authors.
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * https://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.openrewrite.dotnet;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
+import org.openrewrite.ExecutionContext;
+import org.openrewrite.Option;
+import org.openrewrite.RecipeException;
+import org.openrewrite.SourceFile;
+import org.openrewrite.text.PlainText;
+
+import java.io.IOException;
+import java.net.URL;
+import java.nio.charset.Charset;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+@Value
+@EqualsAndHashCode(callSuper = false)
+public class UpgradeAssistantAnalyze extends UpgradeAssistantRecipe {
+    private static final Pattern RECOMMENDATION_SNIPPET_PATTERN =
+            Pattern.compile("(.*)\\n\\nRecommendation:\\n\\n(.*)");
+    private static final Pattern CURRENT_NEW_SNIPPET_PATTERN =
+            Pattern.compile("Current: (.*)\\nNew: (.*)");
+
+    transient UpgradeAssistantAnalysis analysisTable = new UpgradeAssistantAnalysis(this);
+
+    @Option(
+            displayName = "Target framework version",
+            description = "Target framework to which source project should be upgraded.",
+            example = "net9.0")
+    String targetFramework;
+
+    @Override
+    public String getDisplayName() {
+        return "Analyze a .NET project using upgrade-assistant";
+    }
+
+    @Override
+    public String getDescription() {
+        return "Run [upgrade-assistant](https://learn.microsoft.com/en-us/dotnet/core/porting/upgrade-assistant-overview) " +
+                "across a project to analyze changes required to upgrade it to a newer .NET framework version.";
+    }
+
+    @Override
+    public List<String> buildUpgradeAssistantCommand(Accumulator acc, ExecutionContext ctx, Path solutionFile) {
+        List<String> command = new ArrayList<>();
+        command.add(getUpgradeAssistantPath().toString());
+        command.add("analyze");
+        command.add(solutionFile.toString());
+        command.add("--source");
+        command.add("Solution");
+        command.add("--non-interactive");
+        command.add("--targetFramework");
+        command.add(targetFramework);
+        command.add("--serializer");
+        command.add("JSON");
+        command.add("--report");
+        command.add(buildReportPath(acc, solutionFile).getFileName().toString());
+        return command;
+    }
+
+    @Override
+    public void runUpgradeAssistant(Accumulator acc, ExecutionContext ctx) {
+        for (Path solutionFile : acc.getSolutionFiles()) {
+            Path reportPath = buildReportPath(acc, solutionFile);
+            deleteFile(reportPath);
+            execUpgradeAssistant(solutionFile, acc, ctx);
+        }
+    }
+
+    @Override
+    protected void processOutput(Path solutionFile, Path output, Accumulator acc) {
+        Path reportPath = buildReportPath(acc, solutionFile);
+
+        try {
+            ObjectMapper objectMapper = new ObjectMapper();
+            JsonNode reportNode = objectMapper.readTree(reportPath.toFile());
+            JsonNode rulesNode = reportNode.get("rules");
+            Iterator<Map.Entry<String, JsonNode>> iterator = rulesNode.fields();
+            while (iterator.hasNext()) {
+                Map.Entry<String, JsonNode> entry = iterator.next();
+                acc.addRule(entry.getKey(), entry.getValue());
+            }
+
+            for (JsonNode projectNode : reportNode.get("projects")) {
+                for (JsonNode ruleInstanceNode : projectNode.get("ruleInstances")) {
+                    JsonNode locationNode = ruleInstanceNode.get("location");
+                    Path filePath = Paths.get(locationNode.get("path").asText());
+                    acc.addFileResult(acc.getDirectory().resolve(filePath), ruleInstanceNode);
+                }
+            }
+        } catch (IOException e) {
+            throw new RecipeException(e);
+        }
+    }
+
+    private Path buildReportPath(Accumulator acc, Path solutionFile) {
+        String reportName = solutionFile.getFileName().toString().replace(".sln", "-analyze.json");
+        return acc.getDirectory().resolve(reportName);
+    }
+
+    @Override
+    protected SourceFile createAfter(SourceFile before, Accumulator acc, ExecutionContext ctx) {
+        List<JsonNode> results = acc.getFileResults(acc.resolvedPath(before));
+        if (results == null) {
+            return super.createAfter(before, acc, ctx);
+        }
+
+        for (JsonNode ruleInstanceNode : results) {
+            JsonNode locationNode = ruleInstanceNode.get("location");
+            URL link = null;
+            if (locationNode.has("links")) {
+                try {
+                    JsonNode linkNode = locationNode.get("links").get(0);
+                    link = new URL(linkNode.get("url").asText());
+                } catch (IOException e) {
+                    // Ignored
+                }
+            }
+            String projectPath = ruleInstanceNode.get("projectPath").asText();
+            String sourcePath = locationNode.get("path").asText();
+            String ruleId = ruleInstanceNode.get("ruleId").asText();
+            String ruleLabel = acc.getRuleLabel(ruleId);
+            String snippet = locationNode.get("snippet").asText();
+            String codeSnippet;
+            String recommendation;
+            Matcher matcher = RECOMMENDATION_SNIPPET_PATTERN.matcher(snippet);
+            if (matcher.find()) {
+                codeSnippet = matcher.group(1);
+                recommendation = matcher.group(2);
+            } else {
+                matcher = CURRENT_NEW_SNIPPET_PATTERN.matcher(snippet);
+                if (matcher.find()) {
+                    codeSnippet = matcher.group(1);
+                    recommendation = matcher.group(2);
+                } else {
+                    codeSnippet = snippet;
+                    recommendation = null;
+                }
+            }
+
+            analysisTable.insertRow(
+                    ctx,
+                    new UpgradeAssistantAnalysis.Row(projectPath,
+                            sourcePath,
+                            ruleId,
+                            ruleLabel,
+                            codeSnippet,
+                            recommendation,
+                            link));
+        }
+
+        return new PlainText(
+                before.getId(),
+                before.getSourcePath(),
+                before.getMarkers(),
+                Optional.ofNullable(before.getCharset()).map(Charset::name).orElse(null),
+                before.isCharsetBomMarked(),
+                before.getFileAttributes(),
+                null,
+                acc.content(before),
+                Collections.emptyList());
+    }
+}
